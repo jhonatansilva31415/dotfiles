1f9fb51b16ac66ff4b3218f701407dc930c531dd
Jhonatan da Silva <jhonatansilva31415@gmail.com>
2021-04-27 07:09:51 -0300

    xmonad

---
+++ .xmonad/autostart.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-1-1 --mode 2560x1080 --pos 0x0 --rotate normal --output HDMI-1-2 --off --output DP-1-1 --off
+#xmobar &
